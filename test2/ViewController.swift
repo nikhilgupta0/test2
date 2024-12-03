1ed1827832e4a428ed8160c0c3356b37c49d4f74
Nikhil <nikhil.kumargupta0007@gmail.com>
2024-12-03 14:48:29 +0530

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Batch - 1 on 03/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ test2Tests/test2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test2Tests.swift
+//  test2Tests
+//
+//  Created by Batch - 1 on 03/12/24.
+//
+
+import Testing
+@testable import test2
+
+struct test2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
